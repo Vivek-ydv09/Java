46c564ff25ce7b06221ff9a6d6f885ff67b927de
Vivek Yadav <vivek1232yd@gmail.com>
2025-03-25 22:16:57 +0530

    example of if statement.

---
+++ If_condition.java
@@ -0,0 +1,19 @@
+public class If_condition
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		if(a>0)
+		{
+			System.out.println("it is a positive no.");
+		}
+		else if(a<0)
+		{
+			System.out.println("it's negative");
+		}
+		else
+		{
+			System.out.println("it's negative");
+		}
+	}
+}
